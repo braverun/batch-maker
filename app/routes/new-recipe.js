ddbc97e795b1624632862b2134229dd8fa19bdab
Beth Hoskins <bhoskins@charter.net>
2015-03-04 15:25:27 -0500

    commented out code in new-recipes.js route to make sure it worked with the contentSecurityPolicy in place

--- app/routes/new-recipe.js
+++ app/routes/new-recipe.js
@@ -1,15 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: function(){
-    return Ember.RSVP.hash({
-      recipes: this.store.findAll('recipe'),
-      // users: Ember.RSVP.resolve([])
-    });
-  },
+  //   model: function(){
+  //   return Ember.RSVP.hash({
+  //     recipes: this.store.findAll('recipe'),
+  //     // users: Ember.RSVP.resolve([])
+  //   });
+  // },
 
-  setupController: function(controller, model){
-    controller.set('model', model.recipes);
-    // controller.set('users', model.users);
-  }
+  // setupController: function(controller, model){
+  //   controller.set('model', model.recipes);
+  //   // controller.set('users', model.users);
+  // }
 });
