83d39364a75ba8996542ed3cf13e04536dacf58d
Beth Hoskins <bhoskins@charter.net>
2015-03-05 07:19:16 -0500

    initializer parse keys changed from Beth's (for testing) to Weston's

--- app/initializers/parse-token.js
+++ app/initializers/parse-token.js
@@ -4,12 +4,18 @@ export function initialize(/* container, application */) {
   // application.inject('route', 'foo', 'service:foo');
     Ember.$.ajaxSetup({
     headers: {
-      "X-Parse-Application-Id": "Cg7ixMBSyHJ7SsGTqXxUkE27s6PwNevbovd1RaG1",
-      "X-Parse-REST-API-Key": "JzKJm6qSleHdYUXIiAMetmC6ruZYHoqsiHrm4Z8y"
+
+      //Weston's keys
+
+      "X-Parse-Application-Id": "En0HvlSzhghm8rQbbxEudy4jff7J84lvC7C2tWKh",
+      "X-Parse-REST-API-Key": "B9DuWgGoXgSD5SS8Dktluut9E73E25qba1sDJL09"
+
+      //Beth's parse keys for testing
+      // "X-Parse-Application-Id": "Cg7ixMBSyHJ7SsGTqXxUkE27s6PwNevbovd1RaG1",
+      // "X-Parse-REST-API-Key": "JzKJm6qSleHdYUXIiAMetmC6ruZYHoqsiHrm4Z8y"
 
 
-      // "X-Parse-Application-Id": "En0HvlSzhghm8rQbbxEudy4jff7J84lvC7C2tWKh",
-      // "X-Parse-REST-API-Key": "B9DuWgGoXgSD5SS8Dktluut9E73E25qba1sDJL09"
+
     }
   });
 }
